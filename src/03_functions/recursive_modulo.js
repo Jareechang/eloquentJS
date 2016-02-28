750ba07e487ac660f6b6bb722093376d3696c310
Jerry Chang <therealjerrycan@gmail.com>
2016-02-27 19:07:11 -0800

    updated solution for recursive modulo

---
+++ src/03_functions/recursive_modulo.js
@@ -0,0 +1,15 @@
+function isEven(num){ 
+    if(num == 0){
+        return true;
+    }else if(num < 0){
+        return false;
+    }
+    num -= 2;
+    // NOTE: Remember to return the recursive function from the main function
+    return isEven(num);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+
