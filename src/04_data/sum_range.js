7eb412d3bf20b1768c4c64d3c6fafbbc0d326e4a
Jerry Chang <therealjerrycan@gmail.com>
2016-02-28 19:21:40 -0800

    updated sum, range with step

---
+++ src/04_data/sum_range.js
@@ -0,0 +1,36 @@
+function sum(arr){
+    var total = 0;
+    // New es6 for..of
+    for(var num of arr){
+        total += num;
+    }
+    return total;
+}
+
+function range(start, end, step){
+    var arr = [];
+    var num = start;
+    //Check step 
+    if(!step){
+        step = 1;
+    }
+    // Reverse the elements 
+    if(start > end){
+        [start, end] = [end, start];
+    }
+    //counts elements
+    for(var i = start; i <= end; i++){
+        arr.push(num);
+        num += step;
+    }
+    return arr;
+}
+
+// Range
+
+console.log(range(1,10));
+console.log(range(5,2,-1));
+
+// Sum 
+
+console.log(sum(range(1,10)));
