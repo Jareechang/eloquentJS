a71bdd4a73167b803ab3b7fffabb7c1b799b9dfc
Jerry Chang <therealjerrycan@gmail.com>
2016-03-10 19:01:16 -0800

    updated function name

--- src/05_higher_order/ancestry-filter.js
+++ src/05_higher_order/ancestry-filter.js
@@ -7,10 +7,10 @@ var ancestry = JSON.parse(ANCESTRY_FILE);
  * filters a given array as first arg and a predicate test
  * as second argument
  *
- * @param {Array} [arr] argument for the function
- * @param {Function} test callback predicate function
+ * @param {Array} [arr]  - argument for the function
+ * @param {Function} test - callback predicate function
  * 
- **/
+ */
 
 let filter = (arr,test) => {
    var passed = [];
@@ -22,20 +22,20 @@ let filter = (arr,test) => {
    return passed;
 }
 
-/**   
- *   filterByDate() returns anonymous function
- *   based on input min and max
+/**  
+ *  @method filterByDate
+ *  returns anonymous function based on input min and max
  *
- *   @param {Number} min
- *   @param {Number} max
- *   @return {Function} a function that determines if person is between 
+ *  @param {Number} min - set minimum bound for birth date 
+ *  @param {Number} max - set maximum bound for birth date
+ *  @return {Function} a function that determines if person is between 
  *   given born age range
 */
 
-let filterByDate = (min,max) => {
+let filterByDateBetween = (min,max) => {
     return (person) => { return person.born > min && person.born < max }
 }
 
 // Log individuals born between 1900 and 1925 non-inclusive
 
-console.log(filter(ancestry, filterByDate(1900, 1925)));
+console.log(filter(ancestry, filterByDateBetween(1900, 1925)));
