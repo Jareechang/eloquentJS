f723a41f3df9b6482eae4386a7ea1471283074df
Jerry Chang <therealjerrycan@gmail.com>
2016-03-13 23:16:01 -0700

    updated average life expectancy for each century

---
+++ src/05_higher_order/life-expectancy.js
@@ -0,0 +1,32 @@
+import ANCESTRY_FILE from './ancestry.js';
+
+import {average} from './ancestry-composability.js';
+
+var ancestry = JSON.parse(ANCESTRY_FILE);
+
+var byCentury = {};
+
+var isArray = (arr) => arr instanceof Array;
+var insertValue = (k,v) => byCentury[k].push(v);
+var getLifeExpectancy = arr => arr.map(p => p.died - p.born);
+
+var groupBy = function(pluck = null,p) {
+    let century = Math.ceil(p.died/100);
+    let arrayExist = isArray(byCentury[century]);
+    let arrayDoesntExist = !arrayExist;
+    if(arrayDoesntExist){
+        byCentury[century] = [];
+    }
+    !!pluck ? insertValue(century,pluck(p)) : insertValue(century,p);
+}
+
+// Bind an optional callback to pluck the life expectancy in groupBy function
+
+ancestry.forEach(groupBy.bind(null, p => p.died - p.born));
+
+var averageAgebyCentury = Object.keys(byCentury)
+                                .map(function(c) {  
+                                    return { c : Number(average(byCentury[c]).toFixed(2)) } 
+                                 })
+
+console.log(averageAgebyCentury);
