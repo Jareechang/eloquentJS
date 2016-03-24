bc041357fc80837656302c5f24ad1d22061af86c
Jerry Chang <therealjerrycan@gmail.com>
2016-03-23 22:57:13 -0700

    added critter module

---
+++ src/07_electronic_life/critter.js
@@ -0,0 +1,18 @@
+function randomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+};
+
+var directionNames = "n ne e se s sw w nw".split(" ");
+
+function BouncingCritter() {
+    this.direction = randomElement(directionNames);
+};
+
+BouncingCritter.prototype.act(view) {
+    if(view.look(this.direction) != " ") {
+       this.direction = view.find(" ")  || "s";
+       return {type: "move", direction: this.direction};
+    }
+}
+
+export { BouncingCritter };
