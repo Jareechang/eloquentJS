9d877c83b40ca6eef0fc3a57411e5caa878320e2
Jerry Chang <therealjerrycan@gmail.com>
2016-03-31 22:14:21 -0700

    added a regexp function for converting dashes dates to js date objects

---
+++ src/09_regexp/date_regexp.js
@@ -0,0 +1,13 @@
+
+function findDate(string) {
+    var dateTime = /(\d{1,2})-(\d{1,2})-(\d{4})/;
+    var match = dateTime.exec(string);
+    // Note Javascript's date convention of 0 - 11 for months
+    return new Date(
+        Number(match[3]),
+        Number(match[2]) - 1,
+        Number(match[1])
+    )
+}
+
+console.log(findDate("30-1-2003"));
